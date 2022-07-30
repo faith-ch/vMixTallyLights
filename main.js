7a9f250124044a8a340153710e7debccc6dd7719
Ivaylo Ivanov <ivaylozivanov1@gmail.com>
2022-07-30 19:07:34 +0300

    beta version uploaded

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const { Command } = require("commander");
 const { ConnectionTCP } = require("node-vmix");
+// Import dependencies
 const { SerialPort } = require("serialport");
+const { ReadlineParser } = require("@serialport/parser-readline");
 const program = new Command();
 let ip;
 let comP;
@@ -8,6 +10,11 @@ let timeOut;
 let vMixConnected = false;
 let comPortConnected = false;
 let device;
+let deviceInterface;
+let parser;
+let currentValue = 0;
+let frequency;
+let sentValue;
 
 program
   .name("vMix Tally lights")
@@ -29,10 +36,16 @@ program
     "-cp, --comport <comport number>",
     "The comport of the tally lights device"
   )
+  .option(
+    "-f, --frequency <frequency number>",
+    "The frequency on which the data is going to be send",
+    "1"
+  )
   .action((options) => {
     ip = options.ip;
     comP = options.comport;
     timeOut = options.timeout;
+    frequency = options.frequency;
   });
 program.parse();
 
@@ -75,6 +88,13 @@ if (!parseInt(timeOut) && parseInt(timeOut) != 0) {
 } else {
   timeOut = parseInt(timeOut);
 }
+if (!parseInt(frequency)) {
+  throw new Error(
+    `The value of the frequency option is not a number Or it is 0 or lower!\n PLease specify a valid number for the frequency!`
+  );
+} else {
+  frequency = parseInt(frequency);
+}
 
 async function getDevice() {
   const deviceList = await SerialPort.list();
@@ -86,50 +106,79 @@ async function getDevice() {
   if (!device) {
     throw new Error("The com port, that you specifed, does not exist!");
   }
-  const test = new SerialPort({ path: device.path, baudRate: 9600 });
-test.open()
-  test.on("error", (err) => {console.log(err)})
+  // Defining the serial port
+  deviceInterface = new SerialPort({
+    path: device.path,
+    baudRate: 9600,
+  });
+
+  // The Serial port parser
+  parser = new ReadlineParser();
+  deviceInterface.pipe(parser);
+  // Read the data from the serial port
+  connectionTimeout(timeOut, "com");
+  start();
+  // Write the data to the serial port
 }
-getDevice();
 
-// console.log(device);
+function start() {
+  sendData();
+  parser.on("data", (line) => {
+    if (!comPortConnected) {
+      console.log(line);
+      comPortConnected = true;
+    } else {
+      console.log(line);
+    }
+  });
+}
 
-// const connection = new ConnectionTCP(ip);
-// connectionTimeout(timeOut, "vMix");
-// connection.on("tally", (tally) => {
-//   // Your logic here!
-//   const temp = tally.slice(0, 3);
-//   const cams = temp.split("");
-//   if (cams[0] === "1") {
-//     console.log("Камера 1 е на живо!");
-//   } else if (cams[1] === "1") {
-//     console.log("Камера 2 е на живо!");
-//   } else if (cams[2] === "1") {
-//     console.log("Камера 3 е на живо!");
-//   }
-// });
+const connection = new ConnectionTCP(ip);
+connectionTimeout(timeOut, "vMix");
+connection.on("tally", (tally) => {
+  // Your logic here!
+  const temp = tally.slice(0, 3);
+  const cams = temp.split("");
+  if (cams[0] === "1") {
+    currentValue = 1;
+  } else if (cams[1] === "1") {
+    currentValue = 2;
+  } else if (cams[2] === "1") {
+    currentValue = 3;
+  }
+});
 
-// // Listener for data such as tally
-// connection.on("data", (data) => {
-//   if (!vMixConnected) {
-//     if (data.includes("VERSION")) {
-//       vMixConnected = true;
-//     } else {
-//       throw new Error(data);
-//     }
-//   }
-// });
+// Listener for data such as tally
+connection.on("data", (data) => {
+  if (!vMixConnected) {
+    if (data.includes("VERSION")) {
+      vMixConnected = true;
+    } else {
+      throw new Error(data);
+    }
+  }
+});
 
-// connection.on("connect", () => {
-//   connection.send("SUBSCRIBE TALLY");
-// });
+connection.on("connect", () => {
+  connection.send("SUBSCRIBE TALLY");
+  getDevice();
+});
 
-// function connectionTimeout(timeOut, proccess) {
-//   setTimeout(() => {
-//     if (!vMixConnected && proccess == "vMix") {
-//       throw new Error(`vMix connection request timed out!\n `);
-//     } else if (!comPortConnected && proccess == "com") {
-//       throw new Error(`Couldn't connect to com port!`);
-//     }
-//   }, timeOut * 1000);
-// }
+function connectionTimeout(timeOut, proccess) {
+  setTimeout(() => {
+    if (!vMixConnected && proccess == "vMix") {
+      throw new Error(`vMix connection request timed out!\n `);
+    } else if (!comPortConnected && proccess == "com") {
+      throw new Error(`Couldn't connect to com port!`);
+    }
+  }, timeOut * 1000);
+}
+
+function sendData() {
+  sendData = currentValue;
+  deviceInterface.write(`${currentValue}`);
+  setInterval(() => {
+    sendData = currentValue;
+    deviceInterface.write(`${currentValue}`);
+  }, frequency * 1000);
+}
